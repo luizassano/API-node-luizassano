421b3a2110ff32b9c57589d74822cf0e41f3ec43
Luiz Assano <luizaugusto1974@hotmail.com>
2022-07-05 16:39:40 -0300

    fix/routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import { startDatabase } from "./database";
+import categoriesRouter from './routers/categories.routes'
+import productsRouter from './routers/products.routes'
+
+
+
 const app = express();
 app.use(express.json());
-app.use('/categories', categoriesRouter)
+app.use('/categories',categoriesRouter)
 app.use('/products', productsRouter)
 
 export default app.listen(process.env.PORT, () => {
